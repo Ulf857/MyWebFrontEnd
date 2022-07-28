2cad92cc67c0f248c5bc3b4fd2473ae14156198b
noster <noster.ulf@t-online.de>
2022-07-28 11:37:58 +0200

    Start mit InOut und BankVer1

---
+++ Data/BankCode.cs
@@ -0,0 +1,93 @@
+using System.Collections.Generic;
+
+namespace BankCode
+{ 
+
+    public class Transaction
+    {
+        // Properties
+        public decimal Amount { get; }
+        public DateTime Date { get; }
+        public string Notes { get; }
+
+        // Constructor
+        public Transaction(decimal amount, DateTime date, string note)
+        {
+            this.Amount = amount;
+            this.Date = date;
+            this.Notes = note;
+        }
+    }
+
+
+    public class BankAccount
+    {
+        // Properties
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance
+        {
+            get
+
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+
+
+        }
+        private static int accountNumberSeed = 1234567890;
+        public List<Transaction> allTransactions = new List<Transaction>();
+
+        // Constructor
+        public BankAccount(string name, decimal initialBalance)
+        {
+
+            this.Owner = name;
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance"); //(#4)
+
+        }
+
+        // Functions
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            //(#2)
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            //(#1)
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            //(#3)
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+        public void ShowTransactions()
+       {
+            foreach(Transaction item in allTransactions)
+            {
+                Console.WriteLine($"{item.Notes}: Betrag: {item.Amount}");
+            } 
+       } 
+    }
+}
