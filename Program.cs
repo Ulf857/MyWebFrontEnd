b8f6b8b91869a27c05b1b30ebfcd5c3a3e726443
noster <noster.ulf@t-online.de>
2022-08-21 23:16:38 +0200

    Revert "Kultur ist jetzt statisch auf Deutsch gesetzt"
    This reverts commit dfbc0ba688a177ee4b906e89c0d473fef9ed417e.

--- Program.cs
+++ Program.cs
@@ -7,9 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddLocalization();  //Dienst zum Aendern der Kultur zugefuegt
-
-
 
 var app = builder.Build();
 
@@ -20,8 +17,6 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
-app.UseRequestLocalization("en-US");  //Statisches setzten der Kultur
 
 app.UseHttpsRedirection();
 
